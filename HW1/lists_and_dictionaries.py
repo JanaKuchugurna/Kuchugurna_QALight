02033bb463c9dd8e590b06e531a464f69bd187fd
JanaKuchugurna <iana.kuchugurna@gmail.com>
2023-10-28 16:31:58 +0200

    Lists and dictionaries

---
+++ HW1/lists_and_dictionaries.py
@@ -0,0 +1,11 @@
+list1 = [15, 25, 30, 12]
+print("sum:", sum(list1))
+
+list2 = [25, 11, 13, 11, 18, 25]
+my_set = set(list2)
+print(sum(my_set))
+
+dict= {'salary': 1000}
+dict['salary'] *= 1.5
+print(dict)
+
